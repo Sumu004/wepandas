2f49713d1e04821f58f61b3c67963019227ef36c
Priya <priyankamk2903@gmail.com>
2023-10-17 16:07:58 +0530

    deploying website

---
+++ script.js
@@ -0,0 +1,52 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+    showSlidesManual(slideIndex += n);
+}
+
+function showSlidesManual(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.querySelector(".pandas-header").classList.add("scrolled");
+    } else {
+        document.querySelector(".pandas-header").classList.remove("scrolled");
+    }
+}
+
+
+
